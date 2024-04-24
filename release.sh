3fc1e94a3ef097ee13edd242bb84a1048203b1f7
Yousaf Nabi <yousaf@pactflow.io>
2024-04-24 21:23:51 +0100

    feat: add musl built binaries
    refactor release workflow to match pact-reference

--- release.sh
+++ release.sh
@@ -1,61 +1,126 @@
 #!/bin/bash
 
+set -e
+set -x
+
+RUST_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
+
+source "$RUST_DIR/scripts/gzip-and-sum.sh"
+ARTIFACTS_DIR=${ARTIFACTS_DIR:-"$RUST_DIR/release_artifacts"}
+mkdir -p "$ARTIFACTS_DIR"
+export CARGO_TARGET_DIR=${CARO_TARGET_DIR:-"$RUST_DIR/target"}
+
 if [ $# -lt 1 ]
 then
-    echo "Usage : $0 <Linux|Windows|macOS> <version tag>"
+    echo "Usage : $0 <Linux|Windows|macOS> <cargo flags>"
     exit
 fi
 
-set -e
+APP=pact-protobuf-plugin
+OS=$1
+shift;
+echo Building Release for "$OS"
+# All flags passed to this script are passed to cargo.
+cargo_flags=( "$@" )
+# build_manifest() {
+#     cp pact-plugin.json release_artifacts
+#     NEXT=$(echo "$2" | cut -d\- -f2)
+#     sed -e 's/VERSION=\"0.1.5\"/VERSION=\"'${NEXT}'\"/' scripts/install-plugin.sh > target/artifacts/install-plugin.sh
+#     openssl dgst -sha256 -r $ARTIFACTS_DIR/install-plugin.sh > target/artifacts/install-plugin.sh.sha256
 
-echo Building Release for "$1" - "$2"
+# }
+install_cross() {
+    cargo install cross@0.2.5
+}
 
-cargo clean
-mkdir -p target/artifacts
+build_linux_x86_64() {
+    install_cross
+    cross build --target=x86_64-unknown-linux-musl "${cargo_flags[@]}"
+    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
+        gzip_and_sum \
+            "$CARGO_TARGET_DIR/x86_64-unknown-linux-musl/release/${APP}" \
+            "$ARTIFACTS_DIR/${APP}-linux-x86_64.gz"
 
-case "$1" in
+    fi
+    #cargo clean
+}
+
+build_linux_aarch64() {
+    install_cross
+    cross build --target=aarch64-unknown-linux-musl "${cargo_flags[@]}"
+
+    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
+        gzip_and_sum \
+            "$CARGO_TARGET_DIR/aarch64-unknown-linux-musl/release/${APP}" \
+            "$ARTIFACTS_DIR/${APP}-linux-aarch64.gz"
+    fi
+    #cargo clean
+}
+# Build the x86_64 darwin release
+build_macos_x86_64() {
+    cargo build --target x86_64-apple-darwin "${cargo_flags[@]}"
+
+    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
+        gzip_and_sum \
+            "$CARGO_TARGET_DIR/x86_64-apple-darwin/release/${APP}" \
+            "$ARTIFACTS_DIR/${APP}-osx-x86_64.gz"
+        gzip_and_sum \
+                    "$CARGO_TARGET_DIR/x86_64-apple-darwin/release/${APP}" \
+                    "$ARTIFACTS_DIR/${APP}-macos-x86_64.gz"
+    fi
+}
+
+# Build the aarch64 darwin release
+build_macos_aarch64() {
+    cargo build --target aarch64-apple-darwin "${cargo_flags[@]}"
+
+    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
+        gzip_and_sum \
+            "$CARGO_TARGET_DIR/aarch64-apple-darwin/release/${APP}" \
+            "$ARTIFACTS_DIR/${APP}-osx-aarch64.gz"
+        gzip_and_sum \
+                    "$CARGO_TARGET_DIR/aarch64-apple-darwin/release/${APP}" \
+                    "$ARTIFACTS_DIR/${APP}-macos-aarch64.gz"
+    fi
+}
+
+# Build the x86_64 windows release
+build_windows_x86_64() {
+    cargo build --target x86_64-pc-windows-msvc "${cargo_flags[@]}"
+
+    # If --release in cargo flags, then gzip and sum the release artifacts
+    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
+        gzip_and_sum \
+            "$CARGO_TARGET_DIR/x86_64-pc-windows-msvc/release/${APP}.exe" \
+            "$ARTIFACTS_DIR/${APP}-windows-x86_64.exe.gz"
+    fi
+}
+
+# Build the aarch64 windows release
+build_windows_aarch64() {
+    cargo build --target aarch64-pc-windows-msvc "${cargo_flags[@]}"
+
+    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
+        gzip_and_sum \
+            "$CARGO_TARGET_DIR/aarch64-pc-windows-msvc/release/${APP}.exe" \
+            "$ARTIFACTS_DIR/${APP}-windows-aarch64.exe.gz"
+    fi
+}
+
+case "$OS" in
   Linux)    echo "Building for Linux"
-            docker run --rm --user "$(id -u)":"$(id -g)" -v "$(pwd):/workspace" -w /workspace -t \
-              pactfoundation/rust-musl-build -c 'LIBZ_SYS_STATIC=1 cargo build --release'
-            gzip -c target/release/pact-protobuf-plugin > target/artifacts/pact-protobuf-plugin-linux-x86_64.gz
-            openssl dgst -sha256 -r target/artifacts/pact-protobuf-plugin-linux-x86_64.gz > target/artifacts/pact-protobuf-plugin-linux-x86_64.gz.sha256
-            cp pact-plugin.json target/artifacts
-            NEXT=$(echo "$2" | cut -d\- -f2)
-            sed -e 's/VERSION=\"0.1.5\"/VERSION=\"'${NEXT}'\"/' scripts/install-plugin.sh > target/artifacts/install-plugin.sh
-            openssl dgst -sha256 -r target/artifacts/install-plugin.sh > target/artifacts/install-plugin.sh.sha256
-
-            # Build aarch64
-            cargo install cross --git https://github.com/cross-rs/cross
-            cross build --target aarch64-unknown-linux-gnu --release
-            gzip -c target/aarch64-unknown-linux-gnu/release/pact-protobuf-plugin > target/artifacts/pact-protobuf-plugin-linux-aarch64.gz
-            openssl dgst -sha256 -r target/artifacts/pact-protobuf-plugin-linux-aarch64.gz > target/artifacts/pact-protobuf-plugin-linux-aarch64.gz.sha256
+            build_linux_x86_64
+            build_linux_aarch64
             ;;
-  Windows)  echo  "Building for Windows"
-            cargo build --release
-            gzip -c target/release/pact-protobuf-plugin.exe > target/artifacts/pact-protobuf-plugin-windows-x86_64.exe.gz
-            openssl dgst -sha256 -r target/artifacts/pact-protobuf-plugin-windows-x86_64.exe.gz > target/artifacts/pact-protobuf-plugin-windows-x86_64.exe.gz.sha256
+  Windows)  echo "Building for windows"
+            build_windows_x86_64
+            build_windows_aarch64
             ;;
-  macOS)    echo  "Building for OSX"
-            cargo build --release
-            gzip -c target/release/pact-protobuf-plugin > target/artifacts/pact-protobuf-plugin-osx-x86_64.gz
-            openssl dgst -sha256 -r target/artifacts/pact-protobuf-plugin-osx-x86_64.gz > target/artifacts/pact-protobuf-plugin-osx-x86_64.gz.sha256
-            # macos
-            gzip -c target/release/pact-protobuf-plugin > target/artifacts/pact-protobuf-plugin-macos-x86_64.gz
-            openssl dgst -sha256 -r target/artifacts/pact-protobuf-plugin-macos-x86_64.gz > target/artifacts/pact-protobuf-plugin-macos-x86_64.gz.sha256
-
-            # M1
-            # export SDKROOT=$(xcrun -sdk macosx11.1 --show-sdk-path)
-            # export MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx11.1 --show-sdk-platform-version)
-            export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-12}
-            cargo build --target aarch64-apple-darwin --release
-
-            gzip -c target/aarch64-apple-darwin/release/pact-protobuf-plugin > target/artifacts/pact-protobuf-plugin-osx-aarch64.gz
-            openssl dgst -sha256 -r target/artifacts/pact-protobuf-plugin-osx-aarch64.gz > target/artifacts/pact-protobuf-plugin-osx-aarch64.gz.sha256
-            # macos
-            gzip -c target/aarch64-apple-darwin/release/pact-protobuf-plugin > target/artifacts/pact-protobuf-plugin-macos-aarch64.gz
-            openssl dgst -sha256 -r target/artifacts/pact-protobuf-plugin-macos-aarch64.gz > target/artifacts/pact-protobuf-plugin-macos-aarch64.gz.sha256
+  macOS)    echo  "Building for macos"
+            build_macos_x86_64
+            build_macos_aarch64
             ;;
-  *)        echo "$1 is not a recognised OS"
+  *)        echo "$OS is not a recognised OS"
             exit 1
             ;;
 esac
